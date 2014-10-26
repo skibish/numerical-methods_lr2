e3c5bfee840d66ab6a58e537baf0f033000d9362
Ryuzaki0-0 <jurijs.nesterovs@inbox.lv>
2014-10-26 21:01:25 +0200

    Izmenil index.html, dobavil tam vivod svoih rezultatov, i dobil least squares.js, dumaju dolzno bitj pravilno, esli net to budu peredelivatj

--- least-squares.js
+++ least-squares.js
@@ -1,19 +1,17 @@
-//k, N
-//
+var K = 6, N = 7, exerciseNumber = 2, exercise, matrixes, fx = createMultiArray(), b = createEmptyArray(), a = createEmptyArray();
 
-
-function executeMNK(exerciseNumber) {
-	var exOne, exTwo;
+function executeMNK() {
 	switch(exerciseNumber) {
 		case 1:
-			exOne = exerciseOne();
+			exercise = exerciseOne();
 		break;
 		case 2:
-			exTwo = exerciseTwo();
+			exercise = exerciseTwo();
 		break;
 	}
 	
-	console.log(exOne);
+	mnk();
+	outputResult();
 }
 
 function exerciseOne() {
@@ -23,7 +21,7 @@ function exerciseOne() {
 	//posle togo kak poluchili vse znachenija vozvrashaem masiv
 	xValues = range(-2, 2, 0.5);
 	for(item in xValues) {
-		y = Math.round((Math.sin(5 * xValues[item]) * (e ^ xValues[item])) * 1000) / 1000;
+		y = Math.round((Math.sin(5 * xValues[item]) * Math.pow(e, xValues[item])) * 1000) / 1000;
 		outputValues[0][item] = xValues[item];
 		outputValues[1][item] = y;
 	}
@@ -54,20 +52,160 @@ function exerciseTwo() {
 	return table;
 }
 
+function filledMatrix(values) {
+	for(var i = 0; i < K; i++) {
+		for(var j = 0; j < K; j++) {
+			fx[i][j] = 0;
+			for(var k = 0; k < N; k++) {
+				fx[i][j] += Math.pow(values[0][k], (i+j));
+			}
+		}
+	}
+	
+	for(var i = 0; i < K; i++) {
+		for(var j = 0; j < N; j++) {
+			b[i] += (Math.pow(values[0][j], i) * values[1][j]);
+		}
+	}
+	return [fx, b];
+}
+
+function mainGaussWithZeros() {
+	for (var i = 0; i < K; i++) {
+		for (var j = i + 1; j < K; j++) {
+			var temp3 = -matrixes[0][j][i] / matrixes[0][i][i];
+			for (var k = i; k < K; k++) {
+				matrixes[0][j][k] = matrixes[0][j][k] + temp3 * matrixes[0][i][k];
+			}
+			matrixes[1][j] = matrixes[1][j] + temp3 * matrixes[1][i];
+		}
+	}
+	return matrixes;
+}
+
+function reverseGauss() {	
+	for(var i = K - 1; i >= 0; i--){
+		var sum = 0;
+		for(var j = i; j < K; j++){
+			sum += matrixes[0][i][j] * a[j];
+		}
+		
+		a[i] = (matrixes[1][i] - sum) / matrixes[0][i][i];
+	}
+	return matrixes;
+}
+
+function mnk() {
+	matrixes = filledMatrix(exercise);
+	matrixes = mainGaussWithZeros(matrixes);
+	matrixes = reverseGauss(matrixes);
+}
+
 function range(start, stop, step) {
 	var n = start, outputRange = [], i = 1;
 	outputRange[0] = start;
 	
 	while(n < stop) {
-		//etot if postavil dlja togo stobi izbavitsja ot dvuh lishnih znachenij, tak kak nam
-		// nado tolko 7 znahcenij, verno?
 		n += step;
-		if(Math.abs(n) != 1.5) {
-			outputRange[i] = n;
-			i++;
-		}
+		outputRange[i] = n;
+		i++;
 	}
 	return outputRange;
 }
 
-executeMNK(1);
+function createMultiArray() {
+	var matrix = [];
+	for(var i = 0; i < K; i++) {
+		matrix.push([]);
+	}
+	return matrix;
+}
+
+function createEmptyArray() {
+	var matrix = [];
+	for(var i = 0; i < K; i++) {
+		matrix[i] = 0;
+	}
+	return matrix;
+}
+
+function outputResult() {
+	var html = '								\
+		<style type="text/css"> 				\
+			table {								\
+				width: 500px;					\
+				border: 1px solid green;		\
+			}									\
+			td {								\
+				border: 1px solid green; 		\
+				width: 20px;					\
+				padding: 5px;					\
+			}									\
+			h3, p {								\
+				padding: 0;						\
+				margin: 0;						\
+			}									\
+			span.superscript {					\
+				vertical-align: super;			\
+			}									\
+		</style>								\
+												\
+		<h3>System at a given order:</h3>		\
+		<table cellspacing="0" cellpadding="0"> \
+	';
+	
+	for(var i = 0; i < K; i++) {
+		html += '<tr>';
+		for(var j = 0; j < K; j++) {
+			html += '<td>' + matrixes[0][i][j] + '</td>';
+		}
+		html += '<td style="background-color: red;">' + matrixes[1][i] + '</td>';
+		html += '</tr>';
+	}
+	
+	html += '										\
+		</table>									\
+		<br />										\
+		<h3>Coefficients <i>a</i></h3>\
+		<table cellspacing="0" cellpadding="0">		\
+		<tr>										\
+	';
+	
+	for(var u = 0; u < K; u++) {
+		html += '<td>' + a[u] + '</td>';
+	}
+	html += '										\
+		</tr>										\
+		</table>									\
+		<br />										\
+		<h3>X and Y values</h3>						\
+		<table cellspacing="0" cellpadding="0">		\
+	';
+	for(var y = 0; y < 2; y++) {
+		html += '<tr><td>' + (y == 0 ? 'X' : 'Y') + '</td>';
+		for(var t = 0; t < N; t++) {
+			html += '<td>' + exercise[y][t] + '</td>';
+		}
+		html += '</tr>';
+	}
+	html += '							\
+		</table>						\
+		<br />							\
+		<h3>Approximation order</h3>	\
+	';
+	
+	var val = '';
+	for(var x = 0; x < K; x++) {
+		if(x == 0) {
+			val += a[x];
+		} else if(x == 1) {
+			val += (a[x] > 0 ? ' + ' : ' ') + a[x] + 'x ';
+		} else {
+			val += (a[x] > 0 ? ' + ' : ' ') + a[x] + 'x<span class="superscript">' + x + '</span> ';
+		}
+	}
+	html += '<p>' + val + '</p>';
+	document.getElementById('result').innerHTML = html;
+}
+
+executeMNK();
